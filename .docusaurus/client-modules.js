f444358064e3f54c5ba0d3ca64e880ae0625c56e
dararod <dalejorod@gmail.com>
2024-10-11 17:38:39 -0400

    feat: initialize project

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/dalejorod/github.com/http-server-rs.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/home/dalejorod/github.com/http-server-rs.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/dalejorod/github.com/http-server-rs.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/dalejorod/github.com/http-server-rs.github.io/src/css/custom.css"),
+];
