0f51b6e6148380bceea4a4373408292a6f180547
dararod <dalejorod@gmail.com>
2024-10-19 19:20:16 -0400

    remove: ./docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/dalejorod/github.com/http-server-rs.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/dalejorod/github.com/http-server-rs.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/dalejorod/github.com/http-server-rs.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/dalejorod/github.com/http-server-rs.github.io/src/css/custom.css"),
-];
